b47ab1ce9c860297673a2f385f2a6543bb5b5fc8
Jake Wood <jakealistairwood@gmail.com>
2020-05-01 12:29:10 +0100

    robofriends react app

---
+++ build/precache-manifest.f0b06024693b69c1082ccc7dd5046479.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62baf08e22e876caf8f540a1e1ba8499",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ad6c53bab021e0a54df",
+    "url": "/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "be389dd04e9df3d964d4",
+    "url": "/static/css/main.051a399f.chunk.css"
+  },
+  {
+    "revision": "7ad6c53bab021e0a54df",
+    "url": "/static/js/2.479e6b44.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.479e6b44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be389dd04e9df3d964d4",
+    "url": "/static/js/main.7019e519.chunk.js"
+  },
+  {
+    "revision": "46b2ab5944883490d30d",
+    "url": "/static/js/runtime-main.501fdd23.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
